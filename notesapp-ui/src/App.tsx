a40e925f6fedc54f157807ae0dd08bad2a5d68b1
pkliui <kliuiev@pm.me>
2024-10-14 20:42:29 +0300

    add server stuff to front end

--- notesapp-ui/src/App.tsx
+++ notesapp-ui/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 // give notes a type 
@@ -12,44 +12,46 @@ const App = () => {
   //
   // set states
   //
-  // store all notes inside a state
-  // and give note the Note type
-  const [notes, setNotes] = useState<Note[]>([
-    // initialize by adding dummy notes
-    {
-      id: 1,
-      title: "note title 1",
-      content: "content 1",
-    },
-    {
-      id: 2,
-      title: "note title 2",
-      content: "content 2",
-    }
-  ]);
+  // store all notes inside a state and give note the Note type,you may initialize using a dummy array
+  const [notes, setNotes] = useState<Note[]>([]);
 
-  // use state variables for form inputs - enables React to control those inputs
+  // use state variables for form inputs
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
 
   // state variable for a selected note
   const [selectedNote, setSelectedNote] = useState<Note | null>(null);
 
+  // set up API stuff
+  useEffect(()=> {
+    // get notes from DB
+    const fetchNotes = async() => {
+      try{
+        const response = await fetch("http://localhost:5000/api/notes")
+        const notes: Note[] = await response.json()
+        // update notes we just got rom API
+        setNotes(notes)
+      } catch(e){
+        // handle errors that may come from API
+        console.log(e);
+      }
+    };
+    fetchNotes();
+  }, []);
+
   //
   // set handlers
   //
   // handle a click on a note - accepts a note that the user clicked on
-  // to actually know what the selected note (potentially up for a change) is
   const handleNoteClick = (note: Note) => {
     // no need to create a new note, just get the selected one
-    // update title and content accordingly
     setSelectedNote(note);
     setTitle(note.title);
     setContent(note.content);
   };
 
   // handle adding a note
-  const handleAddNote = (
+  const handleAddNote = async (
     // take in event as param, need to say this is of type FormEvent, otherwise TS will complain
     event: React.FormEvent
   ) => {
@@ -58,76 +60,122 @@ const App = () => {
     // console.log("title is ", title)
 
     // create a new note
-    const newNote: Note = {
-      id: notes.length + 1,
-      title: title,
-      content: content
+    //const newNote: Note = {
+    //  id: notes.length + 1,
+    //  title: title,
+    //  content: content
+    //}
+    //
+    // post a new note to the DB
+    try{
+      const response = await fetch
+      ("http://localhost:5000/api/notes",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            title,
+            content
+          })
+        }
+       );
+
+      // receive the note we just posted
+      const newNote = await response.json();
+
+      // update the state with this note 
+      setNotes([newNote, ...notes]);
+      // clean up the submission forms for a nice UI
+      setTitle("");
+      setContent("");
     }
-    // update the state with this note,
-    // pass new array - the (1st) one we just created and the rest of the notes
-    setNotes([newNote, ...notes]);
-    // clean up the submission forms for a nice UI
-    setTitle("");
-    setContent("");
+    catch(error){
+      console.log(error);
+    }
   };
 
-
-  // update a selected note
-  const handleUpdateNote = (
+  // handle updating a selected note
+  const handleUpdateNote = async (
     event: React.FormEvent
   ) => {
     event.preventDefault();
 
     if(!selectedNote){
-      return;
+      return; // if no selected note, just return a function
     }
 
     // actually update the note
-    const updatedNote: Note = {
-      id: selectedNote.id,
-      title: title,
-      content: content,
-    }
+    try{
+      const response = await fetch
+      (`http://localhost:5000/api/notes/${selectedNote.id}`,
+        {
+          method: 'PUT',
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            title,
+            content
+          })
+        }
+      )
 
-    // map function runs thru an array and now we check if we need to update the note (if the id matched selected id)
-    const updatedNotesList = notes.map((note)=>
-      note.id === selectedNote.id // if matches
-      ? updatedNote // update the note
-      : note
-    )
-    // update the notes' state
-    setNotes(updatedNotesList)
-    // upfate the submission form's title and content
-    setTitle("")
-    setContent("")
-    setSelectedNote(null)
+      // receive the note we just updated
+      const updatedNote = await response.json();
+
+      // map function runs thru an array and now we check if we need to update the note (if the id matched selected id)
+      const updatedNotesList = notes.map((note)=>
+        note.id === selectedNote.id // if matches
+        ? updatedNote // update the note
+        : note
+      )
+      // update the notes' state
+      setNotes(updatedNotesList)
+      // upfate the submission form's title and content
+      setTitle("")
+      setContent("")
+      setSelectedNote(null)
+    }catch(error){
+      console.log(error);
+    }
   };
 
-  // reset form
-  const handleCancel = () =>{
+  // handle resetting the form
+  const handleCancel = (event: React.FormEvent) =>{
     setTitle("")
     setContent("")
     setSelectedNote(null);
   }
 
-  // delete a note
-  const deleteNote = (
+  // handle deleting a note
+  const deleteNote = async (
     event: React.MouseEvent,
     noteId: number
   ) => {
     event.stopPropagation();
 
-    const updateNotes = notes.filter(
-      (note) => note.id != noteId
-    )
-    setNotes(updateNotes)
+    try{
+      await fetch(`http://localhost:5000/api/notes/${noteId}`,
+        {
+          method:"DELETE",
+        })
+        }catch(e){
+    }
 
+    // get the filtered notes list
+    const filteredNotesList = notes.filter(
+      (note) => note.id !== noteId)
+    // update the notes' state
+    setNotes(filteredNotesList);
   }
 
   return (
     <div className="app-container">
+      <h1 className="app-title">The Green Notes App</h1>
       {/* 
-      // set up a form for notes 
+      // set up a submission form for notes 
       */}
       <form 
         className="note-form"
@@ -139,7 +187,7 @@ const App = () => {
         }
       >
         {/* 
-        // set up form inputs and text area
+        // set up form's title and content area
         */}
         <input 
           // title input - binded to the title state variable = whatever title state variable is
@@ -148,7 +196,7 @@ const App = () => {
           onChange={(event)=>
             setTitle(event.target.value)
           }
-          placeholder="Title" required
+          placeholder="What do you want to note?" required
         ></input>
         <textarea 
         // text area - binded to the content state variable
@@ -157,19 +205,17 @@ const App = () => {
           onChange={(event)=>
             setContent(event.target.value)
           }
-        placeholder="Content" rows={10} required
+        placeholder="Anything else you forgot to add?" rows={10}
         ></textarea>
 
         {selectedNote ? (
           // handle button logic depending if a note was selected or not 
           <div className="edit-buttons">
-            <button type="submit">Save</button>
+            <button type="submit">Save edits</button>
             <button onClick={handleCancel}>Cancel</button>
           </div>
         ):(
           <button type="submit">Add Note</button>)}
-
-        
       </form>
 
       {/* 
@@ -178,17 +224,20 @@ const App = () => {
       <div className="notes-grid">
         {notes.map((note)=>(
             //Display all notes using the markup below by means of the map function
-            // Will run e.g. 4 times for 4 notes
-            <div className="note-item"
+            <div 
+              className="note-item" onClick={() => handleNoteClick(note)}
               // add an existing note on a click
-              onClick={() => handleNoteClick(note)}
             >
-              <div className="notes-header">
+              <div 
+                className="notes-header" onClick={() => handleNoteClick(note)}
+                // add an existing note on a click
+              >
                 <button
                   // delete button
-                  onClick={(event) => 
-                    deleteNote(event, note.id)
-                  }>x</button>
+                  onClick={(event) => deleteNote(event, note.id)}
+                >
+                  X
+                </button>
               </div>
               <h2>{note.title}</h2>
               <p>{note.content}</p>
